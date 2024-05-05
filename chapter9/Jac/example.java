97720dcae8fdc6d0b3f12ef7aea1a99914b18284
Rachi3 <rlagusdn715@gmail.com>
2024-05-05 22:40:43 +0900

    챕터9 다시 되돌리기 : 패키지와 접근제어자

---
+++ chapter9/Jac/example.java
@@ -0,0 +1,7 @@
+package chapter9.Jac;
+import chapter9.c.*;
+
+public class example {
+    public static void main(String[] args) {
+        }
+}
---
+++ chapter9/c/Package.java
@@ -0,0 +1,13 @@
+package chapter9.c;
+import chapter9.Jac.*;
+
+public class Package{
+    int x;
+    public static void main(String[] args) {
+        System.out.println("Package class.");
+    }
+
+    public void setX(int x){
+        this.x=x;
+    }
+}
