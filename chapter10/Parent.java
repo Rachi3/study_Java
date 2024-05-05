a06f6934fe6414df765913031071110056130de6
Rachi3 <rlagusdn715@gmail.com>
2024-05-05 22:40:58 +0900

    상속 연습

---
+++ chapter10/Parent.java
@@ -0,0 +1,10 @@
+
+public class Parent {
+    public Parent(){
+        System.out.println("Parent Constructor");
+    }
+
+    public void pringName(){
+        System.out.println("Parent printName()");
+    }
+}
