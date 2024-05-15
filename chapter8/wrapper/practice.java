f0ca47c3340a70844d12a6dba1ff95b24e0ba128
Rachi3 <rlagusdn715@gmail.com>
2024-05-16 00:00:14 +0900

    new와 valueOf

--- chapter8/wrapper/practice.java
+++ chapter8/wrapper/practice.java
@@ -1,21 +1,23 @@
 class practice{
     public static void main(String[] args) {
-        Integer i1=new Integer(5);
-        Integer i2=new Integer(5);
+        // Integer i1=new Integer(5);
+        // Integer i2=new Integer(5);
         Integer i3=Integer.valueOf(5);
         Integer i4=Integer.valueOf(5);
 
+        Integer i5=Integer.valueOf(150);
+        Integer i6=Integer.valueOf(150);
 
-        System.out.println(i1==i2);
-        System.out.println(i3==i4);
-        
+        //System.out.println(i1==i2);
+        //System.out.println(i3==i4);
+        System.out.println(i5==i6);
 
-        String s1=new String("hi");
-        String s2=new String("hi");
-        System.out.println(s1.hashCode());
-        System.out.println(s2.hashCode());
+        // String s1=new String("hi");
+        // String s2=new String("hi");
+        // System.out.println(s1.hashCode());
+        // System.out.println(s2.hashCode());
 
-        s1="ih";
-        System.out.println(s1.hashCode());
+        // s1="ih";
+        // System.out.println(s1.hashCode());
     }
 }
